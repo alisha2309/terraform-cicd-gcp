e272688c87e0e729d533bcbd0f56788c4926963b
alisha2309 <57587774+alisha2309@users.noreply.github.com>
2025-02-26 15:32:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "google" {
   credentials = file("/workspace/terraform-service-account.json")
-  project     = "your-project-id"
+  project     = "integral-iris-449816-g3"
   region      = "us-central1"
 }
 
 resource "google_storage_bucket" "my_bucket" {
-  name     = "my-unique-bucket-name-1234"
+  name     = "my_bucket_cicd"
   location = "US"
 }
